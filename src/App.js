fdd8a79c02b148106f95b8db6ac1ecf41c6e210d
philzilla <philippe.yim@gmail.com>
2023-04-17 10:39:34 +0200

    Ajoute le composant Map
    npm i leaflet leaflet-defaulticon-compatibility react-leaflet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {Map} from './componants/Map' 
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <div className="row">
         <div className="col">
           <h1>Pokémon Go !</h1>
+          <Map />
         </div>
       </div>
     </div>
