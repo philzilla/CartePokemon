bbf46e93214568ef1a6e32097056b838be6ff971
philzilla <philippe.yim@gmail.com>
2023-04-17 10:22:59 +0200

    ajoute bootstrap
    penser à installer dep. boostrap et react-bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          dfgfdgfdgfd React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Pokémon Go !</h1>
+        </div>
+      </div>
     </div>
   );
 }
